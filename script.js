fa8ae1b85a895dfdc22c94b50396ac423cbdb719
Jdog2001 <134169267+JoshHill1@users.noreply.github.com>
2023-07-06 21:20:14 -0700

    Styled the Webpage to a dark theme

--- script.js
+++ script.js
@@ -55,14 +55,12 @@ function generatePassword() {
 }
 
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
